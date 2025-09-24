8bd2f18f0d46cd89f40996f34622e7fe4f89c465
AndresFForeroP <andres2200500@correo.uis.edu.co>
2025-09-24 23:49:44 +0000

    feat: :sparkles: Puesto logo central

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Open Sans', 'sans-serif'],
+        slab: ['Roboto Slab', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+}
