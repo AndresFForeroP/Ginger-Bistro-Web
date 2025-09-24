9230f2b44ffec95a0e6ef57a26df7b4628a75532
AndresFForeroP <andres2200500@correo.uis.edu.co>
2025-09-24 20:43:42 +0000

    fix: :bug: Problema arreglado con el js

--- src/menu.js
+++
@@ -1,6 +0,0 @@
-const menuBtn = document.querySelector("nav button");
-const menu = document.getElementById("menu");
-
-menuBtn.addEventListener("click", () => {
-  menu.classList.toggle("hidden");
-});
