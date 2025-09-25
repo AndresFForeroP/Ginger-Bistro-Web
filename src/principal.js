ecc423d28074095ddea912383445c6a1b634459b
AndresFForeroP <andres2200500@correo.uis.edu.co>
2025-09-25 01:16:31 +0000

    feat: :sparkles: Agregados detalles y separado el javascript

---
+++ src/principal.js
@@ -0,0 +1,51 @@
+function getRotationDegrees(element) {
+  const style = window.getComputedStyle(element);
+  const transform = style.getPropertyValue("transform");
+
+  if (transform === "none") return 0;
+
+  const values = transform.split("(")[1].split(")")[0].split(",");
+  const a = values[0];
+  const b = values[1];
+  let angle = Math.round(Math.atan2(b, a) * (180 / Math.PI));
+
+  return (angle < 0 ? angle + 360 : angle);
+}
+
+const menuBtn = document.querySelector("nav button");
+const menu = document.getElementById("menu");
+menuBtn.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
+  if (getRotationDegrees(menuBtn) === 90) {
+    menuBtn.style.transition = "1s";
+    menuBtn.style.transform = "rotateZ(0deg)";
+    menuBtn.classList.remove("bg-gray-500");
+    menuBtn.classList.remove("p-3");
+    menuBtn.classList.remove("rounded-4xl");
+  } else {
+    menuBtn.style.transition = "1s";
+    menuBtn.style.transform = "rotateZ(90deg)";
+    menuBtn.classList.add("bg-gray-500");
+    menuBtn.classList.add("p-3");
+    menuBtn.classList.add("rounded-4xl");
+  }
+});
+
+const card = document.getElementById("card");
+const logo = card.querySelector(".logo");
+const titulo = card.querySelector(".titulo");
+const textos = card.querySelectorAll(".texto");
+
+card.addEventListener("click", () => {
+  card.classList.toggle("pb-20");
+  logo.classList.toggle("w-80");
+  titulo.classList.toggle("text-[14px]");
+  titulo.classList.toggle("opacity-100");
+  titulo.classList.toggle("scale-100");
+
+  textos.forEach(texto => {
+    texto.classList.toggle("text-[14px]");
+    texto.classList.toggle("opacity-100");
+    texto.classList.toggle("scale-100");
+  });
+});
